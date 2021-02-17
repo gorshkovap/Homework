cf75765145db06c8cf658f05e4dd3890f00d53ff
gorshkovap <73563584+gorshkovap@users.noreply.github.com>
2021-02-17 23:34:13 +0300

    Update 2.1.cpp

--- HW2/2.1.cpp
+++ HW2/2.1.cpp
@@ -3,9 +3,18 @@
 #include <string>
 
 int main() {
-	const int n = 153999999; // вылезает ошибка о нехватке памяти
+	const int n = 154999999; // вылезает ошибка о нехватке памяти
 	std::vector<int> vector(n);
 	std::cout << "capacity(): " << vector.capacity() << std::endl;
+	try
+	{
+		if (n > 153999999)
+			throw "There is not the enought space to cover it";
+	}
+	catch (const int* exception)
+	{
+		std::cerr << "Error: " << exception << "\n";
+	}
 
 	std::vector <std::string> sentence;
 	sentence.push_back("Hello");
@@ -13,10 +22,10 @@ int main() {
 	std::cout << "max_size(): " << sentence.max_size() << std::endl;
 	std::cout << "size(): " << sentence.size() << std::endl;
 	std::cout << "capacity(): " << sentence.capacity() << std::endl;
-	sentence.reserve(6);
 	sentence.insert(find(sentence.begin(), sentence.end(), "you?"), "always or not");
 	sentence.insert(sentence.begin(), ("There", "is", "a coomon text"));
 	sentence.back() = "!";
 	std::cout << "size(): " << sentence.size() << std::endl;
 	std::cout << "capacity(): " << sentence.capacity() << std::endl;
+
 }
