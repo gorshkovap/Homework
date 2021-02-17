2dfdcb4c8d1c3535f09fa5f9ebb4da9b7e10efa0
gorshkovap <73563584+gorshkovap@users.noreply.github.com>
2021-02-17 21:20:09 +0300

    Update 2.1.cpp

--- HW2/2.1.cpp
+++ HW2/2.1.cpp
@@ -3,6 +3,10 @@
 #include <string>
 
 int main() {
+	const int n = 152999999; // вылезает ошибка о нехватке памяти
+	std::vector<int> vector(n);
+	std::cout << "capacity(): " << vector.capacity() << std::endl;
+
 	std::vector <std::string> sentence;
 	sentence.push_back("Hello");
 	sentence.insert(sentence.end(), ("How", "are", "you?"));
