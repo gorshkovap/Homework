a828577835c253dc07c3f7415eb1c07ef6022ea5
Александр Горшков <gorshkov.ap@phystech.edu>
2021-03-21 12:16:10 +0300

    added

---
+++ HW_6/6.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <locale>
+#include <sstream>
+
+#include <Windows.h> 
+
+
+
+int main(int argc, char** argv)
+{
+	std::cout << "hello";
+}
